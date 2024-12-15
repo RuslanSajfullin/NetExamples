a3f75a5400a77b84e89275a76ac4c16d81a8ef99
Ruslan Sajfullin <sajfullin.r@gmail.com>
2024-12-15 21:05:10 +0300

    add project Swagger

---
+++ NetExamples/MySwagger/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MySwagger.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- NetExamples/NetExamples.AppHost/Program.cs
+++ NetExamples/NetExamples.AppHost/Program.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.NetExamples>("netexamples");
 
+builder.AddProject<Projects.Swagger>("swagger");
+
+builder.AddProject<Projects.SwaggerCustom>("swaggercustom");
+
 builder.Build().Run();
